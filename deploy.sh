f761e273e0287d62468d1c71b15d80005c38fc68
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2019-03-14 14:42:15 +0100

    ss3332

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,24 @@ setup_git() {
 }
 
 commit_website_files() {
-  cd $HOME/build/devility11/commit-remove/ && ls -la  
-  cd $HOME && ls -la
-  cd $TRAVIS_BUILD_DIR/drupal/modules && ls -la
-  #git add -A
-  #git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  cd $HOME/build/devility11/commit-remove/ 
+  ls -la  
+  cd $HOME 
+  ls -la
+  cd $TRAVIS_BUILD_DIR/drupal/modules 
+  ls -la
+  
 }
 
 upload_files() {
+  #git add -A
+  #git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
   #git push -f -q https://devility11:${GITHUB_TOKEN}@github.com/devility11/commit-remove.git live > /dev/null 2>&1
   
 }
 
 setup_git
 commit_website_files
-upload_files
+#upload_files
 
     
