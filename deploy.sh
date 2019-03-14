f1d0410614066710098c4f55dade5e6543fab1e0
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2019-03-14 13:05:05 +0100

    sssss

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,11 @@ git config --global user.email "norbert.czirjak@oeaw.ac.at"
 git config --global user.name "nczirjak-acdh"
 
 # deploy
-ls -la
 cd modules/oeaw/
+ls -la
 git init
+git checkout -b gh-pages   
 git add -A
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://devility11:$GITHUB_TOKEN@$github.com/devility11/commit-remove.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://devility11:$GITHUB_TOKEN@$github.com/devility11/commit-remove.git" gh-pages > /dev/null 2>&1
+
