8d2653ee87dbee8de88c2f9cd278ce264a510c45
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2019-03-14 15:45:56 +0100

    ssss

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,15 @@ commit_website_files() {
   cd $HOME/build/devility11/commit-remove/  
   rsync -av --progress $HOME/build/devility11/commit-remove/ $HOME/build/devility11/commit-remove/oeaw_commit --exclude $HOME/build/devility11/commit-remove/drupal/  
   cd $HOME/build/devility11/commit-remove/oeaw_commit/
+  ls -la
   git add $HOME/build/devility11/commit-remove/oeaw_commit/* -A
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-  
+  git push -f -q https://devility11:${GITHUB_TOKEN}@github.com/devility11/commit-remove.git live > /dev/null 2>&1
 }
 
 upload_files() {
   
-  git push -f -q https://devility11:${GITHUB_TOKEN}@github.com/devility11/commit-remove.git live > /dev/null 2>&1
+  
   
 }
 
