102205a55b484a1e928356da0e4c7e90ccce263a
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2019-03-15 08:42:38 +0100

    150319.5

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,13 @@ setup_git() {
 }
 
 commit_website_files() {
-  cd $HOME/build/devility11/commit-remove/
-  cp -rf $HOME/build/devility11/commit-remove/src/ $HOME/oeaw_commit/
+  cd $TRAVIS_BUILD_DIR/drupal/modules/oeaw/src/ 
+  cat OeawFunctions.php
+
+  cp -rf $TRAVIS_BUILD_DIR/drupal/modules/oeaw/src/ $HOME/oeaw_commit/
   cd $HOME/oeaw_commit/src
   git init  
-  ls -la
-  cat OeawFunctions.php
+  ls -la  
   git add -A
   git commit --message "FIXED COMMIT"  
   git push -f -q https://devility11:${GITHUB_TOKEN}@github.com/devility11/commit-remove.git live > /dev/null 2>&1
