460b1537c9e86a09b1de1c8cd0e2fe6f451b501e
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2019-03-21 10:34:45 +0100

    unit tests3

--- tests/src/Unit/Model/OeawResourceCustomDataTest.php
+++ tests/src/Unit/Model/OeawResourceCustomDataTest.php
@@ -31,7 +31,7 @@ class OeawResourceCustomDataTest extends UnitTestCase
     }
     
   protected function setUp() {
-        $this->cfgDir = $_SERVER['TRAVIS_BUILD_DIR']."/drupal/modules/oeaw/config.unittest.ini";
+        $this->cfgDir = $_SERVER['TRAVIS_BUILD_DIR']."/config.unittest.ini";
         //we need to setup the configfactory with the "oeaw.settings" config, because of
         // the multilanguage support.
          $this->config = $this->getMockBuilder('\Drupal\Core\Config\ImmutableConfig')
@@ -69,7 +69,7 @@ class OeawResourceCustomDataTest extends UnitTestCase
     
     public function testInitialization() : \Drupal\oeaw\Model\OeawResourceCustomData
     {
-        $obj = new \Drupal\oeaw\Model\OeawResourceCustomData(self::$arrayObject);
+        $obj = new \Drupal\oeaw\Model\OeawResourceCustomData(self::$arrayObject, $this->cfgDir);
         $this->assertInstanceOf(\Drupal\oeaw\Model\OeawResourceCustomData::class, $obj);
         return $obj;
     }
